8da76b6c0dcc53a2a105bdcd03bd0a8f9b4b8574
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-01 15:20:36 +0100

    minor fixes

--- routes/hello.js
+++ routes/hello.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 
 router.get("/", async (req, res) => {
   try {
-    res.status(200).json("Go to https://github/sanchez9aa/MartianRobots if you want to know how this api works")
+    res.status(200).json("Go to https://github.com/sanchez9aa/MartianRobots if you want to know how this api works")
   } catch (err) {
     res.status(500).json({ message: err, success: false });
   }
