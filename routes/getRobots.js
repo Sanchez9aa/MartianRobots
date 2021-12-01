618be6d397ef320bd2b1250d869e139782fcdc9c
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-01 14:38:43 +0100

    endpoint for get all robots and robot by id

---
+++ routes/getRobots.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const Robot = require("../models/Robots")
+
+//Get all robots
+router.get("/", async (req, res) => {
+  try {
+    const robots = await Robot.find({lost:true})
+    res.status(200).json(robots)
+  } catch (err) {
+    res.status(500).json({ message: err, success: false });
+  }
+})
+
+//Get robots by id
+router.get("/:id", async (req, res) => {
+  try {
+    const robots = await Robot.find({_id: req.params.id})
+    res.status(200).json(robots)
+  } catch (err) {
+    res.status(500).json({ message: err, success: false });
+  }
+})
+
+
+module.exports = router
